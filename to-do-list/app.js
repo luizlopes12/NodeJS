371147165b09d2e5af1771ad27e2c3e9bc81b9ab
Luiz Lopes <luizlopes12@outlook.com>
2021-12-19 17:06:56 -0300

    Intalação do express e nodemon

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -1,1 +1,13 @@
+//importando o express
 const express = require('express');
+const nodemon = require('nodemon')
+//colocando a função dentro de uma variavel
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('<h1>Lista de tarefas<h1>');
+});
+
+app.listen(3000, () =>{
+    console.log('Servidor foi iniciado')
+})
