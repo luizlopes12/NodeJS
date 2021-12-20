b829c4e7547181d5465d7937c65b2d7befb4e7ed
Luiz Lopes <luizlopes12@outlook.com>
2021-12-20 09:54:17 -0300

    Update app.js

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const nodemon = require('nodemon')
 //colocando a função dentro de uma variavel
 const app = express();
-
+//Resposta pro client quando o servidor tiver iniciado
 app.get('/', (req, res) =>{
     res.send('<h1>Lista de tarefas<h1>');
 });
-
+//Mostrando que o servidor já iniciou e está funcionando na porta 3000
 app.listen(3000, () =>{
     console.log('Servidor foi iniciado')
 })
