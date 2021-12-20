45d84ff7fac2428dcd8e11dca5512b5cb369b194
Luiz Lopes <luizlopes12@outlook.com>
2021-12-20 11:42:21 -0300

    Update checklist.js

--- to-do-list/src/routes/checklist.js
+++ to-do-list/src/routes/checklist.js
@@ -10,5 +10,9 @@ router.post('/', (req, res) =>{
     console.log(req.body)
     res.status(200).send(req.body);
 })
+router.get('/:id',(req, res)=>{
+    console.log(req.params.id)
+    res.send(`id: ${req.params.id}`)
+})
 //exportando a função
 module.exports = router;
