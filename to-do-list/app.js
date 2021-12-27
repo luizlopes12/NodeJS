70744c41066ac73a46378ed3ca9863d8502cb6b5
Luiz Lopes <luizlopes12@outlook.com>
2021-12-27 10:00:56 -0300

    router ejs

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -4,6 +4,8 @@ const path = require('path')
 require('./config/database')
 //importando a função
 const checkListRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
+
 //O nodemon é um servidor de desenvolvimento, para fazer testes e tal, iniciamos ele com npx nodemon ou nodemon
 const nodemon = require('nodemon');
 const req = require('express/lib/request');
@@ -13,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
+app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
 //Mostrando que o servidor já iniciou e está funcionando na porta 3000
 app.listen(3000, () =>{
